c908b8c476bc115a870914c688e6986baf8fb1a8
unknown <Administrator@.(none)>
2009-10-17 08:54:56 +0800

    '.'

--- rakefile.rb
+++ rakefile.rb
@@ -2,3 +2,11 @@ task :default do
 	sh "git commit -a -m '.'"
 	sh "git push origin master"
 end
+
+task :lf do
+  Dir["*.htm"].each do |f|
+    d=IO.read(f).gsub(/\r\n/,"\n").gsub("\r","\n")
+    puts f
+    open(f,"w"){|f|f<<d}
+  end  
+end  
